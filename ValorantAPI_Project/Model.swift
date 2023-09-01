8deb791504aa9420f37b5bfafd93d308c4aebcc0
Salih Yusuf Göktaş <sygoktas@outlook.com.tr>
2023-09-01 18:41:20 +0300

    Initial Commit

---
+++ ValorantAPI_Project/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  ValorantAPI_Project
+//
+//  Created by Salih Yusuf Göktaş on 1.09.2023.
+//
+
+import Foundation
+
+struct Valorant:Codable {
+	let displayName:String
+	let displayIcon:String
+}
